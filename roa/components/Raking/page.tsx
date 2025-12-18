402d250f1497d5e631be531c40ccf192ef9834ed
Clara <claramikaellyads.1@gmail.com>
2025-12-17 22:37:41 -0300

    adc:synopsis

--- roa/components/Raking/page.tsx
+++ roa/components/Raking/page.tsx
@@ -16,254 +16,236 @@ export default function Ranking() {
       title: "uzumaki",
       type: "anime",
       Temporada: "1",
-      NotaG: 5.5,
-      NotaC: 5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 5.5,
+      NotaMika: 5,
+      synopsis: "Ekirie tenta escapar de sua cidade, Kurouzu-cho, onde os moradores ficam obcecados por espirais devido a uma maldição inexplicável."
     },
     {
       title: "Akame ga kill",
       type: "anime",
       Temporada: "1",
-      NotaG: 7.5,
-      NotaC: 7.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 7.5,
+      NotaMika: 7.5,
+      synopsis: "O jovem Tatsumi viaja para a capital do Império para ganhar dinheiro para seu povo faminto e acaba se envolvendo com a Night Raid, um grupo de assassinos que luta contra a corrupção do governo."
     },
     {
       title: "You",
       type: "Serie",
       Temporada: "1",
-      NotaG: 9.5,
-      NotaC: 9,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 9.5,
+      NotaMika: 9,
+      synopsis: "Série de suspense psicológico que acompanha Joe Goldberg, um homem aparentemente gentil que se torna perigosamente obcecado pelas pessoas que ama."
     },
     {
       title: "Re Life",
       type: "anime",
       Temporada: "1",
-      NotaG: 8,
-      NotaC: 8.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 8,
+      NotaMika: 8.5,
+      synopsis: "Arata Kaizaki recebe a chance de refazer a vida ao voltar ao ensino médio por um ano, enfrentando amadurecimento, amizades e escolhas difíceis."
     },
     {
       title: "Chainsaw Man",
       type: "anime",
       Temporada: "1",
-      NotaG: 9,
-      NotaC: 8,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 9,
+      NotaMika: 8,
+      synopsis: "Denji se funde ao demônio Pochita e passa a caçar demônios como uma arma humana em um mundo violento e cruel."
     },
     {
       title: "Grand Blue",
       type: "anime",
       Temporada: "1",
-      NotaG: 7.5,
-      NotaC: 8.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 7.5,
+      NotaMika: 8.5,
+      synopsis: "Iori Kitahara entra para um clube de mergulho e acaba vivendo situações caóticas cheias de festas, exageros e amizade."
     },
     {
       title: "Cyberpunk Edgerunners",
       type: "anime",
       Temporada: "1",
-      NotaG: 10,
-      NotaC: 9,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 10,
+      NotaMika: 9,
+      synopsis: "David Martinez tenta sobreviver em Night City e se envolve com mercenários em uma história trágica sobre ambição, perda e humanidade."
     },
     {
       title: "Exterminio 2",
       type: "filme",
       Temporada: "1",
-      NotaG: 7.5,
-      NotaC: 7,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 7.5,
+      NotaMika: 7,
+      synopsis: "Após o suposto controle do vírus da raiva, uma falha humana provoca um novo surto que mergulha Londres novamente no caos."
     },
     {
       title: "Metal Lords",
       type: "filme",
       Temporada: "1",
-      NotaG: 7,
-      NotaC: 9.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 7,
+      NotaMika: 9.5,
+      synopsis: "Dois adolescentes formam uma banda de heavy metal para competir em um festival escolar, lidando com amizade e identidade."
     },
     {
       title: "O serviço de entregas da kiki",
       type: "filme",
       Temporada: "1",
-      NotaG: 6,
-      NotaC: 10,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 6,
+      NotaMika: 10,
+      synopsis: "Kiki, uma jovem bruxa, cria um serviço de entregas enquanto aprende a viver sozinha e a confiar em si mesma."
     },
     // {
     //   title: "Black Mirror",
     //   type: "serie",
     //   Temporada: "1",
-    //   NotaG: 8,
-    //   NotaC: 8,
-    //   synopsis:
-    //     "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+    //   NotaGugu: 8,
+    //   NotaMika: 8,
+    //   synopsis: "Série antológica que explora os impactos sombrios da tecnologia na sociedade moderna."
     // },
     {
       title: "Jogo da Morte",
       type: "serie",
       Temporada: "1",
-      NotaG: 6.5,
-      NotaC: 8,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 6.5,
+      NotaMika: 8,
+      synopsis: "Após tirar a própria vida, um homem é condenado a viver várias mortes em corpos diferentes, refletindo sobre culpa e redenção."
     },
     {
       title: "Cães de caça",
       type: "serie",
       Temporada: "1",
-      NotaG: 6,
-      NotaC: 8,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 6,
+      NotaMika: 8,
+      synopsis: "Dois jovens lutadores enfrentam agiotas violentos no submundo do crime, unidos por lealdade e justiça."
     },
     {
-      title: "A noiva-cadáver ",
+      title: "A noiva-cadáver",
       type: "filme",
       Temporada: "1",
-      NotaG: 7,
-      NotaC: 10,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 7,
+      NotaMika: 10,
+      synopsis: "Victor se vê dividido entre o mundo dos vivos e dos mortos após se casar acidentalmente com uma noiva do além."
     },
     {
       title: "Premonição 1",
       type: "filme",
       Temporada: "1",
-      NotaG: 8,
-      NotaC: 7,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 8,
+      NotaMika: 7,
+      synopsis: "Um grupo de jovens escapa da morte após uma premonição, mas passa a ser perseguido por um destino inevitável."
     },
     {
       title: "Premonição 3",
       type: "filme",
       Temporada: "1",
-      NotaG: 8.5,
-      NotaC: 8.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 8.5,
+      NotaMika: 8.5,
+      synopsis: "Uma adolescente tenta enganar a morte após prever um acidente, mas eventos trágicos seguem um padrão mortal."
     },
     {
       title: "Premonição 6",
       type: "filme",
       Temporada: "1",
-      NotaG: 8,
-      NotaC: 8,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 8,
+      NotaMika: 8,
+      synopsis: "Uma nova história mostra que interferir no destino pode ter consequências fatais que atravessam gerações."
     },
     {
       title: "Paranorman",
       type: "filme",
       Temporada: "1",
-      NotaG: 6,
-      NotaC: 9,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
+      NotaGugu: 6,
+      NotaMika: 9,
+      synopsis: "Norman, um garoto que fala com os mortos, precisa salvar sua cidade enfrentando fantasmas e preconceitos."
     },
     {
-      title: "Inatividade Paranormal",
-      type: "filme",
-      Temporada: "1",
-      NotaG: 9.5,
-      NotaC: 9,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "Inatividade Paranormal 2",
-      type: "filme",
-      Temporada: "1",
-      NotaG: 9,
-      NotaC: 9,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "Sword art online",
-      type: "anime",
-      Temporada: "1",
-      NotaG: 9,
-      NotaC: 8.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "To be hero",
-      type: "anime",
-      Temporada: "1",
-      NotaG: 9.5,
-      NotaC: 8.75,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "Erased",
-      type: "anime",
-      Temporada: "1",
-      NotaG: 10,
-      NotaC: 8.75,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "Uma fazenda maluca",
-      type: "serie",
-      Temporada: "1",
-      NotaG: 1,
-      NotaC: 7,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "[Rec] 1",
-      type: "filme",
-      Temporada: "1",
-      NotaG: 8.9,
-      NotaC: 7,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "Dandadan",
-      type: "serie",
-      Temporada: "1",
-      NotaG: 7.9,
-      NotaC: 9,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "O cemiterio das almas perdidas",
-      type: "filme",
-      Temporada: "1",
-      NotaG: 6.9,
-      NotaC: 5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
-    {
-      title: "Invocação do mal 4",
-      type: "filme",
-      Temporada: "1",
-      NotaG: 6,
-      NotaC: 7.5,
-      synopsis:
-        "Em uma cidade assombrada por uma maldição de espirais, uma jovem luta para sobreviver",
-    },
+    title: "Inatividade Paranormal",
+    type: "filme",
+    Temporada: "1",
+    NotaGugu: 9.5,
+    NotaMika: 9,
+    synopsis:
+      "Uma paródia de terror que acompanha um casal vivendo eventos sobrenaturais absurdos em sua casa, misturando sustos com humor escrachado."
+  },
+  {
+    title: "Inatividade Paranormal 2",
+    type: "filme",
+    Temporada: "1",
+    NotaGugu: 9,
+    NotaMika: 9,
+    synopsis:
+      "Continuação da paródia que exagera ainda mais os clichês de filmes de terror, com novas situações bizarras e humor irreverente."
+  },
+  {
+    title: "Sword Art Online",
+    type: "anime",
+    Temporada: "1",
+    NotaGugu: 9,
+    NotaMika: 8.5,
+    synopsis:
+      "Jogadores ficam presos em um jogo de realidade virtual onde morrer no game significa morrer na vida real, e precisam lutar para escapar."
+  },
+  {
+    title: "To Be Hero",
+    type: "anime",
+    Temporada: "1",
+    NotaGugu: 9.5,
+    NotaMika: 8.75,
+    synopsis:
+      "Um designer fracassado se torna um herói improvável após um evento bizarro, enfrentando monstros enquanto lida com situações absurdas e emocionais."
+  },
+  {
+    title: "Erased",
+    type: "anime",
+    Temporada: "1",
+    NotaGugu: 10,
+    NotaMika: 8.75,
+    synopsis:
+      "Um homem volta no tempo para sua infância com o objetivo de impedir crimes trágicos e salvar pessoas importantes de seu passado."
+  },
+  {
+    title: "Uma Fazenda Maluca",
+    type: "serie",
+    Temporada: "1",
+    NotaGugu: 1,
+    NotaMika: 7,
+    synopsis:
+      "Série infantil animada que acompanha animais de uma fazenda vivendo situações caóticas e cômicas no dia a dia."
+  },
+  {
+    title: "[Rec] 1",
+    type: "filme",
+    Temporada: "1",
+    NotaGugu: 8.9,
+    NotaMika: 7,
+    synopsis:
+      "Uma repórter e seu cinegrafista ficam presos em um prédio durante uma quarentena causada por uma infecção aterrorizante."
+  },
+  {
+    title: "Dandadan",
+    type: "serie",
+    Temporada: "1",
+    NotaGugu: 7.9,
+    NotaMika: 9,
+    synopsis:
+      "Dois adolescentes se envolvem em batalhas caóticas contra espíritos e alienígenas, misturando ação, humor e romance sobrenatural."
+  },
+  {
+    title: "O Cemitério das Almas Perdidas",
+    type: "filme",
+    Temporada: "1",
+    NotaGugu: 6.9,
+    NotaMika: 5,
+    synopsis:
+      "Um grupo enfrenta forças malignas ligadas a um antigo cemitério, despertando espíritos e horrores enterrados no passado."
+  },
+  {
+    title: "Invocação do Mal 4",
+    type: "filme",
+    Temporada: "1",
+    NotaGugu: 6,
+    NotaMika: 7.5,
+    synopsis:
+      "Os investigadores paranormais Ed e Lorraine Warren enfrentam um novo caso demoníaco que desafia sua fé e limites."
+  },
   ];
 
   //   async function loadReviews() {
@@ -332,17 +314,17 @@ export default function Ranking() {
                 ?.sort((a, b) => {
                   // Calcula o ranking de cada item
                   const rankingA =
-                    a.NotaG && a.NotaC ? (a.NotaG + a.NotaC) / 2 : a.NotaG;
+                    a.NotaGugu && a.NotaMika ? (a.NotaGugu + a.NotaMika) / 2 : a.NotaGugu;
                   const rankingB =
-                    b.NotaG && b.NotaC ? (b.NotaG + b.NotaC) / 2 : a.NotaC;
+                    b.NotaGugu && b.NotaMika ? (b.NotaGugu + b.NotaMika) / 2 : b.NotaMika;
 
                   // Ordena do maior para o menor
                   return rankingB - rankingA;
                 })
                 .map((item, index) => {
                   const rankingFinal =
-                    item.NotaG && item.NotaC
-                      ? (item.NotaG + item.NotaC) / 2
+                    item.NotaGugu && item.NotaMika
+                      ? (item.NotaGugu + item.NotaMika) / 2
                       : null;
 
                   return (
@@ -355,11 +337,11 @@ export default function Ranking() {
                         <span className="text-yellow-500 font-bold mx-1 ">
                           {rankingFinal?.toFixed(1)}
                           <span className="text-purple-900 mx-5 font-bold">
-                            Nota C: {item.NotaC}{" "}
+                            Nota Mika: {item.NotaMika}{" "}
                             <span className="font-bold mx-5">+</span>
                           </span>
                           <span className=" font-bold text-cyan-600">
-                            Nota G: {item.NotaG}
+                            Nota Gugu: {item.NotaGugu}
                           </span>
                         </span>
                       </p>
