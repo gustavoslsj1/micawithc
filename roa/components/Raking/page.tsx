717d219ff46afcaa9519b0872eb3bc1aa9f090de
Gustavo Lima Sales <102685940+gustavoslsj1@users.noreply.github.com>
2026-01-10 01:31:58 -0300

    estilização e fixed new options and atributes

--- roa/components/Raking/page.tsx
+++ roa/components/Raking/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Trophy, Star, TrendingUp } from "lucide-react";
-
+import { Trophy, Star, TrendingUp, BadgeCheckIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Badge } from "@/components/ui/badge";
 export default function Ranking() {
   //   const [reviews, setReviews] = useState<Review[]>([]);
   //   const [loading, setLoading] = useState(true);
@@ -18,7 +26,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 5.5,
       NotaMika: 5,
-      synopsis: "Ekirie tenta escapar de sua cidade, Kurouzu-cho, onde os moradores ficam obcecados por espirais devido a uma maldição inexplicável."
+      synopsis:
+        "Ekirie tenta escapar de sua cidade, Kurouzu-cho, onde os moradores ficam obcecados por espirais devido a uma maldição inexplicável.",
     },
     {
       title: "Akame ga kill",
@@ -26,15 +35,20 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 7.5,
       NotaMika: 7.5,
-      synopsis: "O jovem Tatsumi viaja para a capital do Império para ganhar dinheiro para seu povo faminto e acaba se envolvendo com a Night Raid, um grupo de assassinos que luta contra a corrupção do governo."
+      synopsis:
+        "O jovem Tatsumi viaja para a capital do Império para ganhar dinheiro para seu povo faminto e acaba se envolvendo com a Night Raid, um grupo de assassinos que luta contra a corrupção do governo.",
     },
     {
       title: "You",
       type: "Serie",
       Temporada: "1",
+      checked: true,
+      Ranking: 9,
+      Badge: "verificado",
       NotaGugu: 9.5,
       NotaMika: 9,
-      synopsis: "Série de suspense psicológico que acompanha Joe Goldberg, um homem aparentemente gentil que se torna perigosamente obcecado pelas pessoas que ama."
+      synopsis:
+        "Série de suspense psicológico que acompanha Joe Goldberg, um homem aparentemente gentil que se torna perigosamente obcecado pelas pessoas que ama.",
     },
     {
       title: "Re Life",
@@ -42,7 +56,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 8,
       NotaMika: 8.5,
-      synopsis: "Arata Kaizaki recebe a chance de refazer a vida ao voltar ao ensino médio por um ano, enfrentando amadurecimento, amizades e escolhas difíceis."
+      synopsis:
+        "Arata Kaizaki recebe a chance de refazer a vida ao voltar ao ensino médio por um ano, enfrentando amadurecimento, amizades e escolhas difíceis.",
     },
     {
       title: "Chainsaw Man",
@@ -50,7 +65,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 9,
       NotaMika: 8,
-      synopsis: "Denji se funde ao demônio Pochita e passa a caçar demônios como uma arma humana em um mundo violento e cruel."
+      synopsis:
+        "Denji se funde ao demônio Pochita e passa a caçar demônios como uma arma humana em um mundo violento e cruel.",
     },
     {
       title: "Grand Blue",
@@ -58,7 +74,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 7.5,
       NotaMika: 8.5,
-      synopsis: "Iori Kitahara entra para um clube de mergulho e acaba vivendo situações caóticas cheias de festas, exageros e amizade."
+      synopsis:
+        "Iori Kitahara entra para um clube de mergulho e acaba vivendo situações caóticas cheias de festas, exageros e amizade.",
     },
     {
       title: "Cyberpunk Edgerunners",
@@ -66,7 +83,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 10,
       NotaMika: 9,
-      synopsis: "David Martinez tenta sobreviver em Night City e se envolve com mercenários em uma história trágica sobre ambição, perda e humanidade."
+      synopsis:
+        "David Martinez tenta sobreviver em Night City e se envolve com mercenários em uma história trágica sobre ambição, perda e humanidade.",
     },
     {
       title: "Exterminio 2",
@@ -74,7 +92,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 7.5,
       NotaMika: 7,
-      synopsis: "Após o suposto controle do vírus da raiva, uma falha humana provoca um novo surto que mergulha Londres novamente no caos."
+      synopsis:
+        "Após o suposto controle do vírus da raiva, uma falha humana provoca um novo surto que mergulha Londres novamente no caos.",
     },
     {
       title: "Metal Lords",
@@ -82,7 +101,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 7,
       NotaMika: 9.5,
-      synopsis: "Dois adolescentes formam uma banda de heavy metal para competir em um festival escolar, lidando com amizade e identidade."
+      synopsis:
+        "Dois adolescentes formam uma banda de heavy metal para competir em um festival escolar, lidando com amizade e identidade.",
     },
     {
       title: "O serviço de entregas da kiki",
@@ -90,7 +110,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 6,
       NotaMika: 10,
-      synopsis: "Kiki, uma jovem bruxa, cria um serviço de entregas enquanto aprende a viver sozinha e a confiar em si mesma."
+      synopsis:
+        "Kiki, uma jovem bruxa, cria um serviço de entregas enquanto aprende a viver sozinha e a confiar em si mesma.",
     },
     // {
     //   title: "Black Mirror",
@@ -106,7 +127,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 6.5,
       NotaMika: 8,
-      synopsis: "Após tirar a própria vida, um homem é condenado a viver várias mortes em corpos diferentes, refletindo sobre culpa e redenção."
+      synopsis:
+        "Após tirar a própria vida, um homem é condenado a viver várias mortes em corpos diferentes, refletindo sobre culpa e redenção.",
     },
     {
       title: "Cães de caça",
@@ -114,7 +136,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 6,
       NotaMika: 8,
-      synopsis: "Dois jovens lutadores enfrentam agiotas violentos no submundo do crime, unidos por lealdade e justiça."
+      synopsis:
+        "Dois jovens lutadores enfrentam agiotas violentos no submundo do crime, unidos por lealdade e justiça.",
     },
     {
       title: "A noiva-cadáver",
@@ -122,7 +145,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 7,
       NotaMika: 10,
-      synopsis: "Victor se vê dividido entre o mundo dos vivos e dos mortos após se casar acidentalmente com uma noiva do além."
+      synopsis:
+        "Victor se vê dividido entre o mundo dos vivos e dos mortos após se casar acidentalmente com uma noiva do além.",
     },
     {
       title: "Premonição 1",
@@ -130,7 +154,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 8,
       NotaMika: 7,
-      synopsis: "Um grupo de jovens escapa da morte após uma premonição, mas passa a ser perseguido por um destino inevitável."
+      synopsis:
+        "Um grupo de jovens escapa da morte após uma premonição, mas passa a ser perseguido por um destino inevitável.",
     },
     {
       title: "Premonição 3",
@@ -138,7 +163,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 8.5,
       NotaMika: 8.5,
-      synopsis: "Uma adolescente tenta enganar a morte após prever um acidente, mas eventos trágicos seguem um padrão mortal."
+      synopsis:
+        "Uma adolescente tenta enganar a morte após prever um acidente, mas eventos trágicos seguem um padrão mortal.",
     },
     {
       title: "Premonição 6",
@@ -146,7 +172,8 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 8,
       NotaMika: 8,
-      synopsis: "Uma nova história mostra que interferir no destino pode ter consequências fatais que atravessam gerações."
+      synopsis:
+        "Uma nova história mostra que interferir no destino pode ter consequências fatais que atravessam gerações.",
     },
     {
       title: "Paranorman",
@@ -154,98 +181,99 @@ export default function Ranking() {
       Temporada: "1",
       NotaGugu: 6,
       NotaMika: 9,
-      synopsis: "Norman, um garoto que fala com os mortos, precisa salvar sua cidade enfrentando fantasmas e preconceitos."
+      synopsis:
+        "Norman, um garoto que fala com os mortos, precisa salvar sua cidade enfrentando fantasmas e preconceitos.",
     },
     {
-    title: "Inatividade Paranormal",
-    type: "filme",
-    Temporada: "1",
-    NotaGugu: 9.5,
-    NotaMika: 9,
-    synopsis:
-      "Uma paródia de terror que acompanha um casal vivendo eventos sobrenaturais absurdos em sua casa, misturando sustos com humor escrachado."
-  },
-  {
-    title: "Inatividade Paranormal 2",
-    type: "filme",
-    Temporada: "1",
-    NotaGugu: 9,
-    NotaMika: 9,
-    synopsis:
-      "Continuação da paródia que exagera ainda mais os clichês de filmes de terror, com novas situações bizarras e humor irreverente."
-  },
-  {
-    title: "Sword Art Online",
-    type: "anime",
-    Temporada: "1",
-    NotaGugu: 9,
-    NotaMika: 8.5,
-    synopsis:
-      "Jogadores ficam presos em um jogo de realidade virtual onde morrer no game significa morrer na vida real, e precisam lutar para escapar."
-  },
-  {
-    title: "To Be Hero",
-    type: "anime",
-    Temporada: "1",
-    NotaGugu: 9.5,
-    NotaMika: 8.75,
-    synopsis:
-      "Um designer fracassado se torna um herói improvável após um evento bizarro, enfrentando monstros enquanto lida com situações absurdas e emocionais."
-  },
-  {
-    title: "Erased",
-    type: "anime",
-    Temporada: "1",
-    NotaGugu: 10,
-    NotaMika: 8.75,
-    synopsis:
-      "Um homem volta no tempo para sua infância com o objetivo de impedir crimes trágicos e salvar pessoas importantes de seu passado."
-  },
-  {
-    title: "Uma Fazenda Maluca",
-    type: "serie",
-    Temporada: "1",
-    NotaGugu: 1,
-    NotaMika: 7,
-    synopsis:
-      "Série infantil animada que acompanha animais de uma fazenda vivendo situações caóticas e cômicas no dia a dia."
-  },
-  {
-    title: "[Rec] 1",
-    type: "filme",
-    Temporada: "1",
-    NotaGugu: 8.9,
-    NotaMika: 7,
-    synopsis:
-      "Uma repórter e seu cinegrafista ficam presos em um prédio durante uma quarentena causada por uma infecção aterrorizante."
-  },
-  {
-    title: "Dandadan",
-    type: "serie",
-    Temporada: "1",
-    NotaGugu: 7.9,
-    NotaMika: 9,
-    synopsis:
-      "Dois adolescentes se envolvem em batalhas caóticas contra espíritos e alienígenas, misturando ação, humor e romance sobrenatural."
-  },
-  {
-    title: "O Cemitério das Almas Perdidas",
-    type: "filme",
-    Temporada: "1",
-    NotaGugu: 6.9,
-    NotaMika: 5,
-    synopsis:
-      "Um grupo enfrenta forças malignas ligadas a um antigo cemitério, despertando espíritos e horrores enterrados no passado."
-  },
-  {
-    title: "Invocação do Mal 4",
-    type: "filme",
-    Temporada: "1",
-    NotaGugu: 6,
-    NotaMika: 7.5,
-    synopsis:
-      "Os investigadores paranormais Ed e Lorraine Warren enfrentam um novo caso demoníaco que desafia sua fé e limites."
-  },
+      title: "Inatividade Paranormal",
+      type: "filme",
+      Temporada: "1",
+      NotaGugu: 9.5,
+      NotaMika: 9,
+      synopsis:
+        "Uma paródia de terror que acompanha um casal vivendo eventos sobrenaturais absurdos em sua casa, misturando sustos com humor escrachado.",
+    },
+    {
+      title: "Inatividade Paranormal 2",
+      type: "filme",
+      Temporada: "1",
+      NotaGugu: 9,
+      NotaMika: 9,
+      synopsis:
+        "Continuação da paródia que exagera ainda mais os clichês de filmes de terror, com novas situações bizarras e humor irreverente.",
+    },
+    {
+      title: "Sword Art Online",
+      type: "anime",
+      Temporada: "1",
+      NotaGugu: 9,
+      NotaMika: 8.5,
+      synopsis:
+        "Jogadores ficam presos em um jogo de realidade virtual onde morrer no game significa morrer na vida real, e precisam lutar para escapar.",
+    },
+    {
+      title: "To Be Hero",
+      type: "anime",
+      Temporada: "1",
+      NotaGugu: 9.5,
+      NotaMika: 8.75,
+      synopsis:
+        "Um designer fracassado se torna um herói improvável após um evento bizarro, enfrentando monstros enquanto lida com situações absurdas e emocionais.",
+    },
+    {
+      title: "Erased",
+      type: "anime",
+      Temporada: "1",
+      NotaGugu: 10,
+      NotaMika: 8.75,
+      synopsis:
+        "Um homem volta no tempo para sua infância com o objetivo de impedir crimes trágicos e salvar pessoas importantes de seu passado.",
+    },
+    {
+      title: "Uma Fazenda Maluca",
+      type: "serie",
+      Temporada: "1",
+      NotaGugu: 1,
+      NotaMika: 7,
+      synopsis:
+        "Série infantil animada que acompanha animais de uma fazenda vivendo situações caóticas e cômicas no dia a dia.",
+    },
+    {
+      title: "[Rec] 1",
+      type: "filme",
+      Temporada: "1",
+      NotaGugu: 8.9,
+      NotaMika: 7,
+      synopsis:
+        "Uma repórter e seu cinegrafista ficam presos em um prédio durante uma quarentena causada por uma infecção aterrorizante.",
+    },
+    {
+      title: "Dandadan",
+      type: "serie",
+      Temporada: "1",
+      NotaGugu: 7.9,
+      NotaMika: 9,
+      synopsis:
+        "Dois adolescentes se envolvem em batalhas caóticas contra espíritos e alienígenas, misturando ação, humor e romance sobrenatural.",
+    },
+    {
+      title: "O Cemitério das Almas Perdidas",
+      type: "filme",
+      Temporada: "1",
+      NotaGugu: 6.9,
+      NotaMika: 5,
+      synopsis:
+        "Um grupo enfrenta forças malignas ligadas a um antigo cemitério, despertando espíritos e horrores enterrados no passado.",
+    },
+    {
+      title: "Invocação do Mal 4",
+      type: "filme",
+      Temporada: "1",
+      NotaGugu: 6,
+      NotaMika: 7.5,
+      synopsis:
+        "Os investigadores paranormais Ed e Lorraine Warren enfrentam um novo caso demoníaco que desafia sua fé e limites.",
+    },
   ];
 
   //   async function loadReviews() {
@@ -287,7 +315,7 @@ export default function Ranking() {
 
   return (
     <div className="min-h-screen pt-24 pb-16">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4  sm:px-6 lg:px-8">
         <div className="text-center mb-12">
           <div className="inline-flex items-center space-x-2 mb-4 px-4 py-2 bg-yellow-500/10 border border-yellow-500/30 rounded-full">
             <Trophy className="text-yellow-400" size={20} />
@@ -296,6 +324,7 @@ export default function Ranking() {
             </span>
           </div>
 
+          {/* header card secundario  */}
           <h1 className="text-4xl sm:text-5xl font-bold mb-4">
             <span className="bg-gradient-to-r from-yellow-400 via-orange-400 to-red-400 bg-clip-text text-transparent">
               Top Animes e Filmes
@@ -306,17 +335,38 @@ export default function Ranking() {
             Notas Atualizadas • Destaques da Comunidade
           </p>
         </div>
-        <div className="flex flex-col border-2 border-black rounded-2xl p-6">
-          <li>
-            lista de animes e colocações
+        <div className="flex flex-col border-2 border-black rounded-2xl p-6  ring-2 ring-white shadow-2xl shadow-white">
+          <li className="list-none">
+            <h3 className="text-2xl font-bold flex flex-row gap-3 items-center justify-center ">
+              lista de animes e colocações <Star color="yellow" />
+            </h3>
+
+            <div className="flex flex-row gap-4 justify-end my-4 bg-gra">
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Tipo" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="serie">serie</SelectItem>
+                  <SelectItem value="filme">filme</SelectItem>
+                  <SelectItem value="anime">anime</SelectItem>
+                  <SelectItem value="all">todos</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
             <ul className=" gap-4 flex flex-col ">
               {lista
                 ?.sort((a, b) => {
                   // Calcula o ranking de cada item
                   const rankingA =
-                    a.NotaGugu && a.NotaMika ? (a.NotaGugu + a.NotaMika) / 2 : a.NotaGugu;
+                    a.NotaGugu && a.NotaMika
+                      ? (a.NotaGugu + a.NotaMika) / 2
+                      : a.NotaGugu;
                   const rankingB =
-                    b.NotaGugu && b.NotaMika ? (b.NotaGugu + b.NotaMika) / 2 : b.NotaMika;
+                    b.NotaGugu && b.NotaMika
+                      ? (b.NotaGugu + b.NotaMika) / 2
+                      : b.NotaMika;
 
                   // Ordena do maior para o menor
                   return rankingB - rankingA;
@@ -329,20 +379,28 @@ export default function Ranking() {
 
                   return (
                     <li className="border-b-1 m-5" key={index}>
-                      <h1 className="font-bold text-2xl">{item.title}</h1>
+                      <div className="flex flex-row items-center gap-3">
+                        <h1 className="font-bold text-2xl">{item.title}</h1>
+                        <p className="mt-2 font-bold">raking: {item.Ranking}</p>
+                      </div>
                       <h2 className="text-[#C47BE4]">{item.synopsis}</h2>
                       <h3 className="font-bold">Temporada: {item.Temporada}</h3>
-                      <p className="font-bold text-gray-500">
-                        Ranking:
-                        <span className="text-yellow-500 font-bold mx-1 ">
-                          {rankingFinal?.toFixed(1)}
-                          <span className="text-purple-900 mx-5 font-bold">
-                            Nota Mika: {item.NotaMika}{" "}
-                            <span className="font-bold mx-5">+</span>
-                          </span>
-                          <span className=" font-bold text-cyan-600">
-                            Nota Gugu: {item.NotaGugu}
-                          </span>
+                      {item.checked && (
+                        <Badge>
+                          <BadgeCheckIcon className="w-4 h-4" />
+                          {item.Badge}
+                        </Badge>
+                      )}
+                      <p className="font-bold text-gray-500 flex flex-row gap-3">
+                        Nota: {rankingFinal?.toFixed(1)}
+                        <Star />
+                        <span className="text-purple-900 mx-5 font-bold flex flex-row gap-3">
+                          Mika: {item.NotaMika} <Star />{" "}
+                          <span className="font-bold mx-5">+</span>
+                        </span>
+                        <span className=" font-bold text-cyan-600 flex flex-row gap-3">
+                          Gugu: {item.NotaGugu}
+                          <Star />
                         </span>
                       </p>
                     </li>
