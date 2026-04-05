3d10d161913df188d0f3ca07ab3464f0cc2eb0fb
gustavoslsj1 <gustavo6555555@gmail.com>
2026-04-05 20:30:44 -0300

    arrumando bug escondido

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -3,5 +3,5 @@ import { GetContents } from "@/lib/services/content";
 
 export default async function Search() {
   const { data: content } = await GetContents();
-  return <SearchPag content={content} />;
+  return <SearchPag content={content ?? []} />;
 }
